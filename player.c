9b408f5be6c7d0c84c84f93faa1aaa30f509815f
doobran <doobran@gmail.com>
2018-10-03 18:29:49 +1000

    updated

--- player.c
+++ player.c
@@ -10,9 +10,7 @@
 #include <ctype.h>
 
 #define MAXDATASIZE 100 /* max number of bytes we can get at once */
-
 #define ARRAY_SIZE 150
-
 #define PORTNUMBER 12345
 
 
@@ -35,12 +33,13 @@ void gen_player_welcome (void){
 }
 
 void server_connect ( void ){
+	int sockfd, new_fd;  /* listen on sock_fd, new connection on new_fd */
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0); // AF_INET = Internet Protocol v4 Addresses (Family), SOCK_STREAM = TCP, 0 = protocol default
 
 	// // Start/define network services
 	struct sockaddr_in server_address; // address of server
 	server_address.sin_family = AF_INET; // Define protocol
-	server_address.sin_port = htons(MYPORT); // Use defined port
+	server_address.sin_port = htons(PORTNUMBER); // Use defined port
 	server_address.sin_addr.s_addr = INADDR_ANY; // Use any IP address on local machine IE 0.0.0.0 
 
 	if (connect(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
